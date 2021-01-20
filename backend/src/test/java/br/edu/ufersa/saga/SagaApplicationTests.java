6d09c57e95a2ff66d15d3de111db62a03b07e52e
Max Denner <maxdenner027@hotmail.com>
2021-01-20 04:26:05 -0300

    add: configurações iniciais do backend adicionadas.

---
+++ backend/src/test/java/br/edu/ufersa/saga/SagaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufersa.saga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
