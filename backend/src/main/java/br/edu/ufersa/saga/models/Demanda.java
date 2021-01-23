3878d7f1e450b04376b235a8f0e3681acbde21ec
Max Denner <maxdenner027@hotmail.com>
2021-01-23 06:37:06 -0300

    add: modelo da entidade Demanda adicionado.

--- backend/src/main/java/br/edu/ufersa/saga/models/Demanda.java
+++ backend/src/main/java/br/edu/ufersa/saga/models/Demanda.java
@@ -1,5 +1,35 @@
 package br.edu.ufersa.saga.models;
 
-public class Demanda {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "demandas")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Demanda implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_demanda", nullable = false, unique = true)
+	private Long idDemanda;
+
+	@Column(name = "nome_demanda", nullable = false, unique = true)
+	private String nomeDemanda;
 
 }
