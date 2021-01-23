1e2dc89fdba10c6be9e11449baa1aa67574c264e
Max Denner <maxdenner027@hotmail.com>
2021-01-23 06:16:16 -0300

    add: modelo da entidade Encaminhamento adicionado.

--- backend/src/main/java/br/edu/ufersa/saga/models/Encaminhamento.java
+++ backend/src/main/java/br/edu/ufersa/saga/models/Encaminhamento.java
@@ -1,5 +1,35 @@
 package br.edu.ufersa.saga.models;
 
-public class Encaminhamento {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "encaminhamentos")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Encaminhamento implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_encaminhamento", nullable = false, unique = true)
+	private Long idEncaminhamento;
+
+	@Column(name = "nome_encaminhamento", nullable = false, unique = true)
+	private String nomeEncaminhamento;
 
 }
