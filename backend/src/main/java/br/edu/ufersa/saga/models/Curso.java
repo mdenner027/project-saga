15d62409db5dcea727ad1134574a8dfd9c289054
Max Denner <maxdenner027@hotmail.com>
2021-01-23 05:45:38 -0300

    add: modelo da entidade Curso adicionado.

--- backend/src/main/java/br/edu/ufersa/saga/models/Curso.java
+++ backend/src/main/java/br/edu/ufersa/saga/models/Curso.java
@@ -1,5 +1,38 @@
 package br.edu.ufersa.saga.models;
 
-public class Curso {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "cursos")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Curso implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_curso", nullable = false, unique = true)
+	private Long idCurso;
+
+	@Column(name = "cod_mec_curso", nullable = false, unique = true)
+	private Long codMecCurso;
+
+	@Column(name = "nome_curso", nullable = false, unique = true)
+	private String nomeCurso;
+	
 }
