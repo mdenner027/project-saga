f697db4db5d0ee4057042098a1952377a06fe0b5
Max Denner <maxdenner027@hotmail.com>
2021-01-24 01:50:26 -0300

    add: relacionamento entre Demanda e Discente definido.

--- backend/src/main/java/br/edu/universidade/saga/models/Atendimento.java
+++ backend/src/main/java/br/edu/universidade/saga/models/Atendimento.java
@@ -8,10 +8,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Getter;
@@ -39,4 +42,8 @@ public class Atendimento implements Serializable {
 	@Column(name = "data_hora_atendimento", nullable = false)
 	private LocalDateTime dataHoraAtendimento;
 
+	@JsonBackReference
+	@ManyToOne(targetEntity = Demanda.class)
+	@JoinColumn(name = "id_demanda_atendimento", nullable = false)
+	private Demanda demandaAtendimento;
 }
--- backend/src/main/java/br/edu/universidade/saga/models/Demanda.java
+++ backend/src/main/java/br/edu/universidade/saga/models/Demanda.java
@@ -1,13 +1,17 @@
 package br.edu.universidade.saga.models;
 
 import java.io.Serializable;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -31,5 +35,9 @@ public class Demanda implements Serializable {
 
 	@Column(name = "nome_demanda", nullable = false, unique = true)
 	private String nomeDemanda;
+
+	@JsonManagedReference
+	@OneToMany(mappedBy = "demandaAtendimento", targetEntity = Atendimento.class)
+	private Set<Atendimento> atendimentosDemanda;
 
 }
