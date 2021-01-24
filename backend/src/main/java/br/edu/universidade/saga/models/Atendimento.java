3369e8cb7a031d5d1c5d639b68a15f083bec9133
Max Denner <maxdenner027@hotmail.com>
2021-01-24 01:33:10 -0300

    add: modelo da entidade Atendimento adicionado.

--- backend/src/main/java/br/edu/universidade/saga/models/Atendimento.java
+++ backend/src/main/java/br/edu/universidade/saga/models/Atendimento.java
@@ -1,5 +1,42 @@
 package br.edu.universidade.saga.models;
 
-public class Atendimento {
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "atendimentos")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Atendimento implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_atendimento", nullable = false, unique = true)
+	private Long idAtendimento;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	@Column(name = "data_hora_atendimento", nullable = false)
+	private LocalDateTime dataHoraAtendimento;
 
 }
